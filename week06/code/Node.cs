67b1749cbf3d61412bfe86a6a3c577313ea230d7
Rony Reyes <rony@caidci.com>
2026-02-07 21:31:43 -0800

    finalizing the last 2 exercises

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -58,7 +58,10 @@ public class Node
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left == null ? 0 : Left.GetHeight();
+        int rightHeight = Right == null ? 0 : Right.GetHeight();
+
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
+
 }
